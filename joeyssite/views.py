1045d18fd2a871b7ec87d237b804c76fe7f97db9
Joey Santana <joeysantana3@gmail.com>
2023-08-07 23:51:32 -0700

    fixing logging and typos

--- joeyssite/views.py
+++ joeyssite/views.py
@@ -1,17 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import os
-import logging
-
-logging.basicConfig(filename='/home/ubuntu/debug.log', encoding='utf-8', level=logging.DEBUG)
-
-# testing functionality
-login_name = os.getlogin()
-logging.debug("login name is " + login_name)
 
 def hello_world(request):
     instance_id = os.environ.get('INSTANCE_ID')
     print(instance_id)
-    logging.debug('Running hello_world')
-    logging.debug(f"Instance id set to {instance_id}")
     return HttpResponse(f"Hello, World! I'm {instance_id}")
