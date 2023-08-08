c2658e7fab6828ce19851716f28e4365b50e731e
Joey Santana <joeysantana3@gmail.com>
2023-08-07 22:53:22 -0700

    adding instanceid to response

--- joeyssite/views.py
+++ joeyssite/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import os
+
 
 def hello_world(request):
-    return HttpResponse("Hello, World!")
+    instance_id = os.environ.get('INSTANCE_ID')
+    return HttpResponse(f"Hello, World! I'm {instance_id}")
